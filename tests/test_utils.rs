d84b41d4f2dd210c65feeab463835e4b68c26824
Nneji123 <ifeanyinneji777@gmail.com>
2024-03-02 05:04:19 +0100

    chore: renamed unit_tests to test_utils.rs

--- tests/test_utils.rs
+++ tests/test_utils.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use ycombinator_scraper_rust::utils::{validate_company_url, validate_job_url};
+    use ycombinator_scraper_rust::utils::{validate_company_url, validate_job_url, strip_html_tags};
 
     #[test]
     fn test_valid_job_url() {
@@ -33,5 +33,18 @@ mod tests {
             validate_company_url(invalid_url).is_err(),
             "Expected invalid URL"
         );
+    }
+
+    #[test]
+    fn test_strip_html_tags() {
+        // Test input with HTML tags
+        let html_content: &str = "<p>This is <b>bold</b> text.</p>";
+        let expected_output: &str = "This is bold text.";
+
+        // Call the strip_html_tags function with the test input
+        let stripped_text: String = strip_html_tags(html_content);
+
+        // Assert that the output matches the expected output
+        assert_eq!(stripped_text, expected_output);
     }
 }
