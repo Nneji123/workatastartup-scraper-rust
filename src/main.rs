bb261186d4ae55fdbbd57f2d422d8b2968ab0c3a
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-03-03 12:02:05 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,17 @@ use log::info;
 #[tokio::main]
 async fn main() {
     // Create a new instance of Scraper
-    let mut scraper: scraper::Scraper = scraper::Scraper::new();
     let dets: config::Config = config::Config::from_env();
     let username: String = dets.login_username;
     let password: String = dets.login_password;
     // Perform login
-    if let Ok(_) = scraper.login(&username, &password).await {
+    if let Ok(_) = scraper::login(&username, &password).await {
         info!("Login successful");
     } else {
         info!("Login failed");
     }
     let company_url: &str = "https://workatastartup.com/companies/vocode";
-    if let Ok(founders) = scraper.scrape_founders_data(company_url).await {
+    if let Ok(founders) = scraper::scrape_founders_data(company_url).await {
         info!("Scraping founders successful");
         println!("{:?}", founders);
     } else {
@@ -29,7 +28,7 @@ async fn main() {
     }
 
     let job_url: &str = "https://www.workatastartup.com/jobs/64333";
-    if let Ok(jobs) = scraper.scrape_job_data(job_url).await {
+    if let Ok(jobs) = scraper::scrape_job_data(job_url).await {
         info!("Scraping jobs successful!");
         println!("{:?}", jobs);
     } else {
